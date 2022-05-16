1da6cbd40c71308b96a0d526a4a15120aa52a800
Selim M <selimontero02@gmail.com>
2022-05-16 09:46:41 -0500

    Added nginx configuration

---
+++ components/generate_nginx_configuration.py
@@ -0,0 +1,67 @@
+import os
+
+def clearConsole(): return os.system(
+    'cls' if os.name in ('nt', 'dos') else 'clear')
+
+def nginx_config():
+  resume = "y"
+
+  while resume == "y" or resume == "Y":
+    print("Generate a new nginx file")
+    print("1. 1 file (api, admin, page in a file | routes page.com/admin | page.com/api)")
+    print("2. 3 file (api, admin, page in multiple files | routes admin.page.com | api.page.com)")
+
+    option = input("Select an option: ")
+
+    if option == "1":
+      nginx_file = ""
+
+      server_name = input("Enter the server name: ")
+      api_port = input("Enter the port of the api: ")
+      admin_port = input("Enter the port of the admin: ", api_port)
+      page_port = input("Enter the port of the page: ", admin_port)
+
+      with open("templates/nginx_one_file_config.txt", "r") as nginx_config_template:
+        nginx_file = nginx_config_template.read()
+
+      nginx_file = nginx_file.replace("{{SERVER_NAME}}", server_name)
+      nginx_file = nginx_file.replace("{{PAGE_PORT}}", page_port)
+      nginx_file = nginx_file.replace("{{ADMIN_PORT}}", admin_port)
+      nginx_file = nginx_file.replace("{{API_PORT}}", api_port)
+
+      with open(server_name + ".conf", "w") as file:
+        file.write(nginx_file)
+      print("Please allow sudo permissions to move files to /etc/nginx/conf.d/")
+      os.system("sudo mv " + server_name + ".conf /etc/nginx/conf.d/")
+      clearConsole()
+
+      print("\n NGINX CONFIG FILE WAS GENERATED SUCCESSFULLY AND MOVED TO /etc/nginx/conf.d/")
+      resume = "n"
+
+    elif option == "2":
+      multiple_files = ["api","admin","page"]
+
+      for file in multiple_files:
+        file_name = input(f"Do you have a {file}? (y/n):")
+
+        if file_name == "y" or file_name == "Y":
+          nginx_file = ""
+          server_name = input("Enter the server name: ")
+          page_port = input("Enter the port of the page: ")
+
+          with open("templates/nginx_multiple_files_config.txt", "r") as nginx_config_template:
+            nginx_file = nginx_config_template.read()
+
+          nginx_file = nginx_file.replace("{{SERVER_NAME}}", server_name)
+          nginx_file = nginx_file.replace("{{PORT}}", page_port)
+
+          with open(f"{file}.{server_name}" + ".conf", "w") as file:
+            file.write(nginx_file)
+
+          os.system("sudo mv " +  f"{file}.{server_name}" + ".conf /etc/nginx/conf.d/")
+          clearConsole()
+
+        print("\n NGINX CONFIG FILE WAS GENERATED SUCCESSFULLY AND MOVED TO /etc/nginx/conf.d/")
+        resume = "n"
+    else:
+      print("Invalid option")
