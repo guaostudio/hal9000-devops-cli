a1b098218b3148428becff8d23ec11904c67f85d
Seigm <sgarcia@guaodev.com>
2022-06-17 11:11:27 -0500

    in progress

--- components/info_project.py
+++ components/info_project.py
@@ -1,16 +1,34 @@
-def get_info_project(home_path):
-    print("""
-1.- Projects info
-2.- SSH keys info
-3.- System info
-    """)
-    input_option = input(": ")
+from os import name, path
+import json
 
-    if(input_option == '1'):
-        print("Project Info")
-    elif(input_option == '2'):
-        print("SSH Keys Info")
-    elif(input_option == '3'):
-        print("HOME PATH: ", home_path)
-        print("COMMANDS PROJECT: ", )
-    print("home_path: ", home_path)
+
+def get_info_project(home_path):
+    folder_path_slash = ('\\' if name == 'nt' else '/')
+    data_projects_path = home_path + folder_path_slash + "data_projects.json"
+    dash = ('-' * 50)
+
+    fileValidation = path.exists(data_projects_path)
+    data_project = f"""
+    INFO
+HOME PATH: {home_path}
+COMMANDS PATH: {home_path + folder_path_slash + "commands"}
+DATA PROJECTS PATH: {data_projects_path}
+    """
+    if(fileValidation == False):
+        data_project += f"\nPROJECTS: You don't have any project"
+        return print(data_project)
+    else:
+        data_project += f"\nPROJECTS:\n"
+
+    with open(data_projects_path, "r") as jsonFile:
+        file_data = json.load(jsonFile)
+
+    for project in file_data["projects"]:
+        data_project += f"""
+\n{dash}
+Project name: {project["project_name"]}
+    API: {True if project["api"]["url_repository"] else False}
+    PAGE: {True if project["page"]["url_repository"] else False}
+    ADMIN: {True if project["admin"]["url_repository"] else False}
+{dash}\n"""
+    return print(data_project)
