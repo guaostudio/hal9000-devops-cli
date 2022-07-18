d780d4b0b1ed86490b68c14b0df047297716f336
Seigm <sgarcia@guaodev.com>
2022-07-18 16:47:34 -0500

    install cli

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install python3
-sudo apt install python3-pip
+sudo apt install -y python3
+sudo apt install -y python3-pip
 pip install -r requirements.txt
 mkdir ~/.deploy
 touch .env
 echo "CLI_PATH=~/.deploy/CLI/" >> .env
 echo "alias deploy='python3 ~/.deploy/CLI/index.py'" >> ~/.bashrc
 echo "alias update_cli='sh ~/.deploy/CLI/update.sh'" >> ~/.bashrc
-sudo mv ~/CLI ~/.deploy
+sudo cp ~/CLI ~/.deploy
 cd ~
 source .bashrc
+sudo rm -rf ~/CLI
