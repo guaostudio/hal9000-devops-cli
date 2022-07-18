699fe1f4397d7a221d4da4452e3be49a660b1670
Selim Garcia Montero <85191388+Seigm0@users.noreply.github.com>
2022-07-18 16:50:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ touch .env
 echo "CLI_PATH=~/.deploy/CLI/" >> .env
 echo "alias deploy='python3 ~/.deploy/CLI/index.py'" >> ~/.bashrc
 echo "alias update_cli='sh ~/.deploy/CLI/update.sh'" >> ~/.bashrc
-sudo cp ~/CLI ~/.deploy
+sudo cp -r ~/CLI ~/.deploy
 cd ~
 source .bashrc
 sudo rm -rf ~/CLI
